f566533eb0c9f9b9f08b20e47bb55c0eb9bfa894
borish <kd0858053@gmail.com>
2026-09-17 16:46:04 +0530

    first java code

--- first.js
+++ first.js
@@ -1,6 +1,7 @@
 let age = 18;
 if (age >= 18) {
   console.log("Adult");
-} else {
+} else 
+  {
   console.log("Minor");
 }
---
+++ new.js
@@ -0,0 +1,6 @@
+marks = int(input("Marks dalo: "))
+
+if marks >= 33:
+    print("Pass ho gaye")
+else:
+    print("Fail ho gaye")
